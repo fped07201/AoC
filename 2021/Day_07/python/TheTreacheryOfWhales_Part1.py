b28e080b87a819169936a33d8978b0277372211d
Ferran Pedrera <ferran.pedrera@technica-engineering.de>
2022-06-11 11:59:21 +0200

    Add day 7

---
+++ 2021/Day_07/python/TheTreacheryOfWhales_Part1.py
@@ -0,0 +1,14 @@
+with open("../input.txt",'r') as fd:
+    h_pos = [int(x) for x in fd.read().split(',')]
+    h_pos_max = max(h_pos)
+    h_pos_min = min(h_pos)
+
+    min_fuel=None
+    min_pos=None
+    for i in range(h_pos_min, h_pos_max+1):
+        fuel = sum([abs(x-i) for x in h_pos])
+        if min_fuel is None or fuel<min_fuel:
+            min_fuel=fuel
+            min_pos=i
+
+    print("Min fuel %d in position %d"%(min_fuel,min_pos))
